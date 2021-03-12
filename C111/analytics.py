63d32bb5d5ff4e4dfb7ef0132593778db46b9627
sabithasn <63290879+sabithasn@users.noreply.github.com>
2021-03-12 13:05:53 +0530

    Update analytics.py

--- C111/analytics.py
+++ C111/analytics.py
@@ -1,37 +1,38 @@
 import plotly.figure_factory as ff
+import pandas as pd
+import csv
 import plotly.graph_objects as go
 import statistics
 import random
-import pandas as pd 
-import csv
-
-def random_set_of_means(counter):
-    datafile = pd.read_csv("data.csv")
-    data =datafile["Math_score"].tolist()
-    dataset = []
-    for i in range (0, counter):
-        random_index = random.randint(0,len(data)-1)
-        value = data[random_index]
-        dataset.append(value)
-
-    mean = statistics.mean(dataset)
-    return mean
-
-def show_fig(mean_list):
-    datafile= mean_list
-    fig = ff.create_distplot([datafile], ["mean"], show_hist=False)
-    ffig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.20], mode="lines", name="MEAN"))
-    fig.show()
-
-
-def Standard_deviation ():
-    meanlist= []
-    meanlist[0] = "mean"
-    for i in range (0,1000):
-        set_of_means = random_set_of_means(100)
-        mean_list.append(set_of_means)
-    std_deviation=statistics.stdev(mean_list)
-    print( "Standard Deviation of sampling mean :- ", std_deviation)
-    show_fig(mean_list)
-
-Standard_deviation()
+#reading scores data
+df = pd.read_csv("School2.csv")
+data = df["Math_score"].tolist()
+#Calculating the mean and the standard deviation
+mean = sum(data) / len(data)
+std_deviation = statistics.stdev(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+#Finding 1 standard deviation start and end values, and 2 standard deviations start and end values
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
+third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
+#Plotting the chart, and lines for mean, 1 standard deviation and 2 standard deviations
+fig = ff.create_distplot([data], ["Math_score"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_start, first_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 START"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_start, second_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 START"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_start, third_std_deviation_start], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 START"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+#Printing the findings
+list_of_data_within_1_std_deviation = [result for result in data if result > first_std_deviation_start and result < first_std_deviation_end]
+list_of_data_within_2_std_deviation = [result for result in data if result > second_std_deviation_start and result < second_std_deviation_end]
+list_of_data_within_3_std_deviation = [result for result in data if result > third_std_deviation_start and result < third_std_deviation_end]
+print("Mean of this data is {}".format(mean))
+print("Median of this data is {}".format(median))
+print("Mode of this data is {}".format(mode))
+print("Standard deviation of this data is {}".format(std_deviation))
+print("{}% of data lies within 1 standard deviation".format(len(list_of_data_within_1_std_deviation)*100.0/len(data)))
+print("{}% of data lies within 2 standard deviations".format(len(list_of_data_within_2_std_deviation)*100.0/len(data)))
