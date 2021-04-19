ff00d36776ab3e00865fcdbe159fe3faa541732f
sabithasn <63290879+sabithasn@users.noreply.github.com>
2021-04-19 18:54:10 +0530

    Create cdice1.py

---
+++ P108/cdice1.py
@@ -0,0 +1,11 @@
+import random
+import plotly.express as px
+result=[]
+count=[]
+for i in range(0,100):
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    result.append(dice1+dice2)
+    count.append(i)
+fig=px.bar(x=result, y=count)
+fig.show()
